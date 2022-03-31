7dd1e4348a1a52860247c94520f5558461f9c5e1
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2022-03-31 16:27:18 +0100

    + completed video player | added images for github readme example

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ReactPlayer from "react-player";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>React Video Player Demo</p>
-        <div>
-          <input
-            className="form-control"
-            style={{ marginBottom: 20 }}
-            type="text"
-            placeholder="Input Video URL"
-          />
-        </div>
-        <div>
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-            controls={true}
-          />
-        </div>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputValue: "",
+      url: "",
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ inputValue: event.target.value });
+  };
+
+  handleSubmit = (data) => {
+    data.preventDefault();
+    this.setState({ url: this.state.inputValue });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>React Video Player Demo</p>
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <input
+                onChange={this.handleChange}
+                className="form-control"
+                style={{ marginBottom: 20 }}
+                type="text"
+                placeholder="Input Video URL"
+              />
+              <button className="btn btn-primary" style={{ marginBottom: 20 }}>
+                Play Video
+              </button>
+            </form>
+          </div>
+          <div>
+            <ReactPlayer url={this.state.url} controls={true} />
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
